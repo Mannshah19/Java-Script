cb3b3e9a6f2247b68bae2b0a7153e5a0b782186d
Mann Shah <mannshah4798@gmail.com>
2026-05-21 10:48:19 +0530

    Add grade calculator functionality in JavaScript and TypeScript

---
+++ ts/practise2.js
@@ -0,0 +1,23 @@
+"use strict";
+let username = "Mann";
+console.log(username);
+console.log("......Grade Calculator.......!");
+let marks = 85;
+if (marks > 90) {
+    console.log("Grade A+");
+}
+else if (marks > 80) {
+    console.log("Grade A");
+}
+else if (marks > 70) {
+    console.log("Grade B+");
+}
+else if (marks > 60) {
+    console.log("Grade B");
+}
+else if (marks > 50) {
+    console.log("Grade C");
+}
+else {
+    console.log("Fail");
+}
---
+++ ts/practise2.ts
@@ -0,0 +1,35 @@
+let username:string = "Mann";
+console.log(username);
+
+console.log("......Grade Calculator.......!");
+
+let marks:number = 85;
+
+if (marks > 90) {
+    console.log("Grade A+");
+    
+}
+else if (marks > 80)
+{
+    console.log("Grade A");
+}
+
+else if (marks > 70)
+{
+    console.log("Grade B+");
+}
+
+else if (marks > 60)
+{
+    console.log("Grade B");
+}
+
+else if (marks > 50)
+{
+    console.log("Grade C");
+}
+
+else 
+{
+    console.log("Fail");
+}
