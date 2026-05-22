b13eade1d98568f318e2fde4e9decc0855594bcc
Mann Shah <mannshah4798@gmail.com>
2026-05-22 10:13:35 +0530

    Add TypeScript version of variable demonstration using 'any' type

---
+++ ts/project basic calculator/7-variable-using-any-type.js
@@ -0,0 +1,10 @@
+"use strict";
+let data;
+data = 100;
+console.log(data);
+data = "Mann";
+console.log(data);
+data = true;
+console.log(data);
+data = 99.99;
+console.log(data);
---
+++ ts/project basic calculator/7-variable-using-any-type.ts
@@ -0,0 +1,13 @@
+let data: any;
+
+data = 100;
+console.log(data);
+
+data = "Mann";
+console.log(data);
+
+data = true;
+console.log(data);
+
+data = 99.99;
+console.log(data);
